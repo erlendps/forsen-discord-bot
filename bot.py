9895cde1f48f68206301442a79a8c665ff1f5493
Erlend Skaaden <erlendskaaden@gmail.com>
2021-03-13 14:55:17 +0100

    Add bot.py
    Added base functionality

--- bot.py
+++ bot.py
@@ -1,19 +1,38 @@
 # bot.py
 
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="=")
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} is now tracking throwing.')
+    print(f'{bot.user.name} is now tracking throwing.')
 
-client.run(TOKEN)
+@bot.event
+async def on_message(message):
+    if (message.author == bot.user):
+        return
+    
+    forsen_quotes = ["fucking unlucky", "forsene", "zulul", "no, "*10+"no, yes", "guys, relax", "bread boat OMEGALUL",
+                    "hey molly", "The god gamer is back"]
+
+    if "forsen" in message.content.lower():
+        return_message = random.choice(forsen_quotes)
+        await message.channel.send(return_message)
+
+@bot.command(name="forsen")
+async def is_forsen_live():
+    return
+
+
+bot.run(TOKEN)
 
 
